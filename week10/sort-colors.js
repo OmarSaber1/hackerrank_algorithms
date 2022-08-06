05a38dce25172f2d4194726f6e5170da2f783d96
OmarSaber1 <osaber218@gmail.com>
2022-08-06 19:55:35 +0200

    refactro

--- week10/sort-colors.js
+++ week10/sort-colors.js
@@ -1,3 +1,27 @@
-var sortColors = function (nums) {
-  return nums.sort((a, b) => a - b);
-};
+function dutch_flag_sort(arr) {
+  // all elements < low are 0, and all elements > high are 2
+  // all elements from >= low < i are 1
+  let low = 0,
+    high = arr.length - 1,
+    i = 0;
+  while (i <= high) {
+    if (arr[i] === 0) {
+      [arr[i], arr[low]] = [arr[low], arr[i]]; // swap
+      // increment 'i' and 'low'
+      i += 1;
+      low += 1;
+    } else if (arr[i] === 1) {
+      i += 1;
+    } else {
+      // the case for arr[i] === 2
+      [arr[i], arr[high]] = [arr[high], arr[i]]; // swap
+      // decrement 'high' only, after the swap the number at index 'i' could be 0, 1, or 2
+      high -= 1;
+    }
+    console.log(arr, i);
+  }
+}
+
+let arr = [1, 0, 2, 1, 0];
+dutch_flag_sort(arr);
+console.log(arr);
