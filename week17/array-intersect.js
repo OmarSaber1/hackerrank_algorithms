1b899d07f8597812fde7dd0a05f399d7b994ee85
OmarSaber1 <osaber218@gmail.com>
2022-09-13 00:18:45 +0200

    to hex

--- week17/array-intersect.js
+++ week17/array-intersect.js
@@ -1,21 +1,21 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
- */
 var intersect = function (nums1, nums2) {
-  let a1 = nums1.sort((a, b) => a - b);
-  let a2 = nums2.sort((a, b) => a - b);
-  let result = [];
-  while (a1.length && a2.length) {
-    if (a1[0] === a2[0]) {
-      result.push(a1.shift());
-      a2.shift();
-    } else if (a1[0] > a2[0]) {
-      a2.shift();
+  let arr1 = nums1.sort((a, b) => a - b);
+  let arr2 = nums2.sort((a, b) => a - b);
+  const results = [];
+
+  while (arr1.length && arr2.length) {
+    console.log(arr1, arr2);
+    if (arr1[0] === arr2[0]) {
+      results.push(arr1.shift());
+      arr2.shift();
+    } else if (arr1[0] > arr2[0]) {
+      arr2.shift();
     } else {
-      a1.shift();
+      arr1.shift();
     }
   }
-  return result;
+
+  return results;
 };
+
+intersect([4, 9, 5], [9, 4, 9, 8, 4]);
---
+++ week17/to-hex.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var toHex = function (num) {
+  const arr = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
+
+  if (num == 0) return "0";
+
+  if (num < 0) {
+    nums += Math.pow(2, 32);
+  }
+
+  let res = "";
+
+  while (num > 0) {
+    const digit = num % 16;
+    res = arr[digit] + res;
+
+    num = Math.floor(num / 16);
+  }
+
+  return res;
+};
