f24d979d04909ad2e074c4d3a1fd10fb9085d805
OmarSaber1 <osaber218@gmail.com>
2022-09-17 14:14:25 +0200

    combination sum

---
+++ blind75/dynamic-programming/combinationSum.js
@@ -0,0 +1,16 @@
+var combinationSum4 = function (N, T) {
+  const dp = Array(T + 1).fill(0);
+  dp[0] = 1;
+
+  for (let i = 1; i <= T; i++) {
+    for (let num of N) {
+      if (num <= i) {
+        dp[i] += dp[i - num];
+      }
+      console.log(dp, { i }, { num });
+    }
+  }
+
+  return dp[T];
+};
+console.log(combinationSum4([1, 2, 3], 4));
