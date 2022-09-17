77b55333d1c8f9e05ad6280fc2e96e677d1c3ae7
OmarSaber1 <osaber218@gmail.com>
2022-09-17 19:21:57 +0200

    refactor num decode

--- blind75/dynamic-programming/num-decoding.js
+++ blind75/dynamic-programming/num-decoding.js
@@ -1,24 +1,26 @@
-var numDecodings = function (s) {
-  if (s.length < 1) return 0;
-  let memo = [];
-  var recur = function (index) {
-    let result = 0;
-    if (index == s.length) return 1; // base case 1: reach the end = correct path
-    if (memo[index] != null) return memo[index]; // base case 2: if we already have the information on this index, return it
-    if (s[index] > 0) {
-      // as long as its bigger than 0, we can at least  use it as a single digit
-      result += recur(index + 1); // move forward by 1
+var numDecodings = function (s = "") {
+  const dp = Array(s.length + 1).fill(0);
+
+  dp[0] = 1;
+  dp[1] = s.charAt(0) == "0" ? 0 : 1;
+
+  for (var i = 2; i <= s.length; i++) {
+    const oneDigit = Number(s.substring(i - 1, i));
+    const twoDigit = Number(s.substring(i - 2, i));
+
+    if (oneDigit >= 1) {
+      console.log(dp[i], dp[i - 1], dp[i - 2]);
+      dp[i] += dp[i - 1];
     }
 
-    // as long as 1. we are not starting with 0 etc. `04`
-    //            2. our next digit isn't null
-    //            3. we can form a two digit number thats smaller than 27
-    if (s[index] != 0 && s[index + 1] != null && s[index] + s[index + 1] < 27) {
-      // lets make this two digit together and move forward by 2
-      result += recur(index + 2);
+    if (twoDigit >= 10 && twoDigit <= 26) {
+      console.log(oneDigit, twoDigit);
+      dp[i] += dp[i - 2];
     }
-    memo[index] = result; // given the current index, store how many different ways we can decode
-    return result;
-  };
-  return recur(0);
+  }
+
+  console.log(dp);
+  return dp[dp.length - 1];
 };
+
+console.log(numDecodings("12"));
