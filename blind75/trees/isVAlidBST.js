a9ac517549d3ae7a12efe5021a905437dd43242c
OmarSaber1 <osaber218@gmail.com>
2022-09-23 17:44:15 +0200

    is valid BST

---
+++ blind75/trees/isVAlidBST.js
@@ -0,0 +1,10 @@
+var isValidBST = function (root, min = -Infinity, max = Infinity) {
+  if (!root) return true;
+
+  if (root.val >= max || root.val <= min) return false;
+
+  return (
+    isValidBST(root.left, min, root.val) &&
+    isValidBST(root.right, root.val, max)
+  );
+};
