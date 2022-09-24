a9d801b09f757cdf2fda08039140b8be80646a91
OmarSaber1 <osaber218@gmail.com>
2022-09-24 17:15:38 +0200

    merge k list

---
+++ blind75/heap/merge-k-list.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+var mergeKLists = function (lists) {
+  const minHeap = [];
+
+  lists.map((a) => a && minHeap.push(a));
+
+  minHeap.sort((a, b) => b.val - a.val);
+
+  let head = null,
+    tail = null;
+
+  while (minHeap.length > 0) {
+    let node = minHeap.pop();
+
+    if (!head) {
+      head = tail = node;
+    } else {
+      tail.next = node;
+      tail = node;
+    }
+
+    if (node.next) {
+      minHeap.push(node.next);
+    }
+    minHeap.sort((a, b) => b.val - a.val);
+  }
+
+  return head;
+};
