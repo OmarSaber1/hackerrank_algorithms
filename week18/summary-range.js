c42bee96de0f1a9812ac1630d6ced2c51ae3c533
OmarSaber1 <osaber218@gmail.com>
2022-10-01 12:56:33 +0200

    update summary range

--- week18/summary-range.js
+++ week18/summary-range.js
@@ -3,30 +3,14 @@
  * @return {string[]}
  */
 var summaryRanges = function (nums) {
-  if (nums.length == 0) return nums;
+  const res = [];
 
-  if (nums.length == 1) return [`${nums[0]}`];
+  let left = nums[0];
 
-  let left = nums[0],
-    res = [];
-
-  for (var i = 1; i < nums.length; i++) {
-    const curr = nums[i],
-      prev = nums[i - 1];
-
-    if (curr !== prev + 1 && prev == left) {
-      res.push(`${left}`);
-      left = curr;
-    } else if (curr !== prev + 1) {
-      res.push(`${left}->${prev}`);
-      left = curr;
-    }
-
-    if (i == nums.length - 1 && left == nums[i]) {
-      res.push(`${left}`);
-    }
-    if (i == nums.length - 1 && left !== nums[i]) {
-      res.push(`${left}->${nums[i]}`);
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] + 1 !== nums[i + 1]) {
+      res.push(nums[i] == left ? "" + nums[i] : `${left}->${nums[i]}`);
+      left = nums[i + 1];
     }
   }
 
