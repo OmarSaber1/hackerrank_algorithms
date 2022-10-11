a41ac7400829d28876e8aa72f5ed545de4eefa6d
OmarSaber1 <osaber218@gmail.com>
2022-10-11 13:08:50 +0200

    n-arr preorder

---
+++ week21/n-arr-preorder.js
@@ -0,0 +1,23 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function (root, arr = []) {
+  if (!root) return arr;
+
+  arr.push(root.val);
+
+  for (var child of root.children) {
+    preorder(child, arr);
+  }
+
+  return arr;
+};
