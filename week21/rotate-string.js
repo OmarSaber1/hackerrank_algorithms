75a83f5799e0f8fc0bb6289fa38a43e7986b8568
OmarSaber1 <osaber218@gmail.com>
2022-10-12 18:50:18 +0200

    unique morse reprsentation

--- week21/rotate-string.js
+++ week21/rotate-string.js
@@ -3,6 +3,17 @@
  * @param {string} goal
  * @return {boolean}
  */
-var rotateString = function (s, goal) {
-  return s.repeat(2).includes(goal);
+var rotateString = function (A, B) {
+  if (A === B) return true;
+
+  for (let char of A) {
+    A = A.slice(1) + A.slice(0, 1); //rotating the string
+    console.log(A);
+    if (A === B) return true;
+  }
+
+  return false;
+  return s.length === goal.length && s.repeat(2).includes(goal);
 };
+
+rotateString("abcde", "cdeab");
